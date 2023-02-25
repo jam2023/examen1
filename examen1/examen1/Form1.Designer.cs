f43ec7717ef5802ecdd9a3fc0b11b8ba527c4760
Jose Angel <angelmendoza_b2016@hotmail.com>
2023-02-24 18:27:19 -0600

    primer formulario
    primer formulario

---
+++ examen1/examen1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace examen1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.lbMeses = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(383, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CANTIDAD DEPOSITADA";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(363, 87);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 22);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(171, 173);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(425, 173);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // lbMeses
+            // 
+            this.lbMeses.FormattingEnabled = true;
+            this.lbMeses.ItemHeight = 16;
+            this.lbMeses.Location = new System.Drawing.Point(274, 228);
+            this.lbMeses.Name = "lbMeses";
+            this.lbMeses.Size = new System.Drawing.Size(277, 116);
+            this.lbMeses.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbMeses);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "formulario 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ListBox lbMeses;
+    }
+}
+
---
+++ examen1/examen1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace examen1
+{
+    public partial class Form1 : Form
+    {
+        double cantidad = 200000;
+        double interesMensual = 1.5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtCantidad.Text = cantidad.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] meses = {"NINGUNO","ENERO", "FEBRERO", "MARZO",
+                              "ABRIL", "MAYO", "JUNIO",
+                              "JULIO", "AGOSTO", "SEPTIEMBRE",
+                              "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"
+                             };
+            lbMeses.Items.Clear();
+
+            int periodoTiempo = dateTimePicker2.Value.Month;
+            double valorGanado = (cantidad * interesMensual) * periodoTiempo;
+
+            if (dateTimePicker1.Value.Month < dateTimePicker2.Value.Month)
+            {
+                for (int x = Convert.ToInt32(dateTimePicker1.Value.Month); x <= Convert.ToInt32(dateTimePicker2.Value.Month); x++)
+                {
+                    lbMeses.Items.Add(meses[x].ToString() + "-----------" + valorGanado);
+                }
+            }
+            else
+            {
+                for (int x = Convert.ToInt32(dateTimePicker1.Value.Month); x <= 12; x++)
+                {
+                    lbMeses.Items.Add(meses[x].ToString() + "-----------" + valorGanado);
+                }
+                for (int x = 1; x <= Convert.ToInt32(dateTimePicker2.Value.Month); x++)
+                {
+                    lbMeses.Items.Add(meses[x].ToString() + "-----------" + valorGanado);
+                }
+            }
+        }
+    }
+}

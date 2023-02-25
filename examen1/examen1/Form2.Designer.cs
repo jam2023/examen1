600e07b2033ae1ccf032cb34728feb4b9c20bfab
Jose Angel <angelmendoza_b2016@hotmail.com>
2023-02-25 02:41:54 -0600

    formulario3
    formulario3

--- examen1/examen1/Form2.Designer.cs
+++ examen1/examen1/Form2.Designer.cs
@@ -28,19 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.producto1textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.precio1textBox = new System.Windows.Forms.TextBox();
+            this.precio2textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cantidad1textBox = new System.Windows.Forms.TextBox();
+            this.cantidad2textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.subtotaltextBox = new System.Windows.Forms.TextBox();
+            this.porcentajetextBox = new System.Windows.Forms.TextBox();
+            this.documentotextBox = new System.Windows.Forms.TextBox();
+            this.impuestotextBox = new System.Windows.Forms.TextBox();
+            this.totalpagartextBox = new System.Windows.Forms.TextBox();
+            this.producto2textBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(151, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRODUCTOS";
+            // 
+            // producto1textBox
+            // 
+            this.producto1textBox.Location = new System.Drawing.Point(63, 95);
+            this.producto1textBox.Name = "producto1textBox";
+            this.producto1textBox.Size = new System.Drawing.Size(100, 22);
+            this.producto1textBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(239, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PRECIO UNITARIO";
+            // 
+            // precio1textBox
+            // 
+            this.precio1textBox.Location = new System.Drawing.Point(63, 235);
+            this.precio1textBox.Name = "precio1textBox";
+            this.precio1textBox.Size = new System.Drawing.Size(100, 22);
+            this.precio1textBox.TabIndex = 4;
+            // 
+            // precio2textBox
+            // 
+            this.precio2textBox.Location = new System.Drawing.Point(250, 235);
+            this.precio2textBox.Name = "precio2textBox";
+            this.precio2textBox.Size = new System.Drawing.Size(100, 22);
+            this.precio2textBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(134, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "CANTIDADES";
+            // 
+            // cantidad1textBox
+            // 
+            this.cantidad1textBox.Location = new System.Drawing.Point(63, 332);
+            this.cantidad1textBox.Name = "cantidad1textBox";
+            this.cantidad1textBox.Size = new System.Drawing.Size(100, 22);
+            this.cantidad1textBox.TabIndex = 7;
+            // 
+            // cantidad2textBox
+            // 
+            this.cantidad2textBox.Location = new System.Drawing.Point(250, 332);
+            this.cantidad2textBox.Name = "cantidad2textBox";
+            this.cantidad2textBox.Size = new System.Drawing.Size(100, 22);
+            this.cantidad2textBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "FACTURA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 461);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "SUBTOTAL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 500);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "PORCENTAJE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(56, 534);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "DOCUMENTO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(56, 577);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "IMPUESTO";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(38, 623);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(113, 16);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "TOTAL A PAGAR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 495);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 55);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "FACTURAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // subtotaltextBox
+            // 
+            this.subtotaltextBox.Location = new System.Drawing.Point(165, 455);
+            this.subtotaltextBox.Name = "subtotaltextBox";
+            this.subtotaltextBox.Size = new System.Drawing.Size(100, 22);
+            this.subtotaltextBox.TabIndex = 16;
+            // 
+            // porcentajetextBox
+            // 
+            this.porcentajetextBox.Location = new System.Drawing.Point(165, 497);
+            this.porcentajetextBox.Name = "porcentajetextBox";
+            this.porcentajetextBox.Size = new System.Drawing.Size(100, 22);
+            this.porcentajetextBox.TabIndex = 17;
+            // 
+            // documentotextBox
+            // 
+            this.documentotextBox.Location = new System.Drawing.Point(165, 534);
+            this.documentotextBox.Name = "documentotextBox";
+            this.documentotextBox.Size = new System.Drawing.Size(100, 22);
+            this.documentotextBox.TabIndex = 18;
+            // 
+            // impuestotextBox
+            // 
+            this.impuestotextBox.Location = new System.Drawing.Point(165, 577);
+            this.impuestotextBox.Name = "impuestotextBox";
+            this.impuestotextBox.Size = new System.Drawing.Size(100, 22);
+            this.impuestotextBox.TabIndex = 19;
+            // 
+            // totalpagartextBox
+            // 
+            this.totalpagartextBox.Location = new System.Drawing.Point(165, 617);
+            this.totalpagartextBox.Name = "totalpagartextBox";
+            this.totalpagartextBox.Size = new System.Drawing.Size(100, 22);
+            this.totalpagartextBox.TabIndex = 20;
+            // 
+            // producto2textBox
+            // 
+            this.producto2textBox.Location = new System.Drawing.Point(236, 95);
+            this.producto2textBox.Name = "producto2textBox";
+            this.producto2textBox.Size = new System.Drawing.Size(100, 22);
+            this.producto2textBox.TabIndex = 21;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(478, 702);
+            this.Controls.Add(this.producto2textBox);
+            this.Controls.Add(this.totalpagartextBox);
+            this.Controls.Add(this.impuestotextBox);
+            this.Controls.Add(this.documentotextBox);
+            this.Controls.Add(this.porcentajetextBox);
+            this.Controls.Add(this.subtotaltextBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cantidad2textBox);
+            this.Controls.Add(this.cantidad1textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.precio2textBox);
+            this.Controls.Add(this.precio1textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.producto1textBox);
+            this.Controls.Add(this.label1);
             this.Name = "Form2";
-            this.Text = "formulario2";
+            this.Text = "formulario 2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox producto1textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox precio1textBox;
+        private System.Windows.Forms.TextBox precio2textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox cantidad1textBox;
+        private System.Windows.Forms.TextBox cantidad2textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox subtotaltextBox;
+        private System.Windows.Forms.TextBox porcentajetextBox;
+        private System.Windows.Forms.TextBox documentotextBox;
+        private System.Windows.Forms.TextBox impuestotextBox;
+        private System.Windows.Forms.TextBox totalpagartextBox;
+        private System.Windows.Forms.TextBox producto2textBox;
     }
 }
--- examen1/examen1/Form2.cs
+++ examen1/examen1/Form2.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace examen1
 {
     public partial class Form2 : Form
@@ -16,5 +17,47 @@ namespace examen1
         {
             InitializeComponent();
         }
+
+        private async void button1_Click_1(object sender, EventArgs e)
+           
+        {
+            decimal precio1, precio2, subtotal,descuento,isv;
+            int cantidad, cantidad2;
+
+            if (producto1textBox.Text == "" || producto2textBox.Text == "" || precio1textBox.Text == "" ||
+                precio2textBox.Text == "" || cantidad1textBox.Text == "" || cantidad2textBox.Text == "") ;
+            {
+                MessageBox.Show("error complete los espacio en blanco", "aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                precio1 = Convert.ToDecimal(precio1textBox.Text);
+                precio2 = Convert.ToDecimal(precio2textBox.Text);
+                cantidad = Convert.ToInt32(precio1textBox.Text);
+                cantidad2 = Convert.ToInt32(precio1textBox.Text);
+
+                subtotaltextBox.Text= ((precio1 * cantidad)+ (precio2 * cantidad2)).ToString();
+                documentotextBox.Text = await DescuentoAsync(Convert.ToDecimal(subtotaltextBox.Text, Convert.ToDecimal(porcentajetextBox.Text));
+
+                subtotal = Convert.ToDecimal(subtotaltextBox.Text);
+                descuento = Convert.ToDecimal(documentotextBox);
+                decimal porcentaje = Convert.ToDecimal(porcentajetextBox.Text);
+
+                totalpagartextBox.Text= ((subtotal - descuento)* porcentaje).ToString();
+                isv = Convert.ToDecimal(impuestotextBox.Text);
+
+                totalpagartextBox.Text =(subtotal- descuento+ isv).ToString();
+            }
+        }
+        private async Task<string> DescuentoAsync(decimal subtotal, decimal porcentaje)
+        {
+            decimal descuento = await Task.Run(() =>
+            {
+                return (subtotal * porcentaje);
+            });
+            return descuento.ToString();
+        }
+
+
     }
 }
